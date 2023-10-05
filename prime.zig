029b3044d273dc102173d0ff15ac0bcbf67a9a01
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-05 22:58:42 +0900

    add prime

---
+++ prime.zig
@@ -0,0 +1,26 @@
+fn isPrime(number: u16) bool {
+    var i: u16 = 2;
+    while (i < number) : (i += 1) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const std = @import("std");
+
+pub fn main() void {
+    const max: u16 = 100;
+
+    var i: u16 = 3;
+
+    var arrays = [_]u16{2};
+
+    while (i < max) : (i += 1) {
+        if (isPrime(i)) {
+            arrays[arrays.len - 1] = i;
+            std.debug.print("{d}\n", .{arrays[arrays.len - 1]});
+        }
+    }
+}
