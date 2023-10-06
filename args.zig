80611fee0a6d233e9f24f96544b5187edceed353
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-06 19:21:50 +0900

    add args

---
+++ args.zig
@@ -0,0 +1,14 @@
+// args.zig
+
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    std.debug.print("Arguments: {s}\n", .{args});
+}
