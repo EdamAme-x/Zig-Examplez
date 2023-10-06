924273764de198adf5df2c4f1db1e1a7e50ee45a
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-06 17:33:54 +0900

    add input

---
+++ input.zig
@@ -0,0 +1,19 @@
+// input.zig
+
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const stdin = std.io.getStdIn();
+
+    std.debug.print("input: ", .{});
+    var input = std.ArrayList(u8).init(allocator);
+    defer input.deinit();
+
+    try stdin.reader().streamUntilDelimiter(input.writer(), '\n', 1024);
+
+    std.debug.print("input a string: {s}\n", .{input.items});
+}
